a831dedcfd918143e6474c69cd2f7d51f1c46000
HakunaMatataBert <Bert.zhang30@outlook.com>
2018-11-29 15:12:32 +0800

    fds

--- src/com/ls/demo/ArrayListAnalyze.java
+++ src/com/ls/demo/ArrayListAnalyze.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 public class ArrayListAnalyze {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		ArrayList<String> list =  new ArrayList<>();
 		list.add("1");
 		list.add("1");
